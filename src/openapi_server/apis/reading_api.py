356358938ca7a43104e5452e8b7be603858660c2
Laura Porter <laura@dxw.com>
2022-08-23 12:54:19 +0100

    Basic read GET endpoint - get a judgment
    Implement a basic read endpoing to get a judgment. Assume for now that only
    published judgments are requested. Return a 404 if the judgment is not found.

--- src/openapi_server/apis/reading_api.py
+++ src/openapi_server/apis/reading_api.py
@@ -2,6 +2,7 @@
 
 from typing import Dict, List  # noqa: F401
 
+from caselawclient.Client import MarklogicAPIError
 from fastapi import (  # noqa: F401
     APIRouter,
     Body,
@@ -18,6 +19,8 @@ from fastapi import (  # noqa: F401
 from openapi_server.models.extra_models import TokenModel  # noqa: F401
 from openapi_server.security_api import get_token_basic
 
+from openapi_server.connect import client_for_basic_auth
+
 router = APIRouter()
 
 
@@ -25,18 +28,24 @@ router = APIRouter()
     "/judgment/{judgmentUri:path}",
     responses={
         200: {"description": "A single judgment document, in Akoma Ntoso XML"},
+        404: {"description": "Document not found"},
     },
     tags=["Reading"],
     summary="Read a judgment or decision, given its URI",
     response_model_by_alias=True,
 )
 async def get_document_by_uri(
+    response: Response,
     judgmentUri: str = Path(None, description=""),
     token_basic: TokenModel = Security(get_token_basic),
-) -> str:
-    """Unless the client has `read_unpublished_documents` permission,
-    then only published documents are accessible."""
-    return "jqqzdwd"
+):
+    try:
+        client = client_for_basic_auth(token_basic)
+        judgment = client.get_judgment_xml(judgmentUri)
+    except MarklogicAPIError:
+        response.status_code = 404
+        return "Resource not found."
+    return Response(status_code=200, content=judgment, media_type="application/xml")
 
 
 @router.get(
--- tests/test_reading_api.py
+++ tests/test_reading_api.py
@@ -1,27 +1,33 @@
 # coding: utf-8
+from unittest.mock import patch, Mock
 
 import pytest
+from caselawclient.Client import MarklogicResourceNotFoundError
 
 from fastapi.testclient import TestClient
+from openapi_server.main import app
 
 
-@pytest.mark.xfail(reason="Test is TODO")
-def test_get_document_by_uri(client: TestClient):
-    """Test case for get_document_by_uri
-
-    Read a judgment or decision, given its URI
-    """
-
-    headers = {
-        "Authorization": "BasicZm9vOmJhcg==",
-    }
-    response = client.request(
-        "GET",
-        "/{judgmentUri}".format(judgmentUri="judgment_uri_example"),
-        headers=headers,
-    )
-
+@patch("openapi_server.apis.reading_api.client_for_basic_auth")
+def test_get_success(mocked_client):
+    mocked_client.return_value.get_judgment_xml.return_value = b"<judgment></judgment>"
+    response = TestClient(app).request("GET", "/judgment/uri", auth=("user", "pass"))
+    mocked_client.return_value.get_judgment_xml.assert_called_with("uri")
     assert response.status_code == 200
+    assert "<judgment>" in response.text
+
+
+@patch("openapi_server.apis.reading_api.client_for_basic_auth")
+def test_get_not_found(mocked_client):
+    mocked_client.return_value.get_judgment_xml.side_effect = Mock(
+        side_effect=MarklogicResourceNotFoundError()
+    )
+    response = TestClient(app).request(
+        "GET", "/judgment/bad_uri", auth=("user", "pass")
+    )
+    mocked_client.return_value.get_judgment_xml.assert_called_with("bad_uri")
+    assert response.status_code == 404
+    assert "Resource not found." in response.text
 
 
 @pytest.mark.xfail(reason="Test is TODO")
